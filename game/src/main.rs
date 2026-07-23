156849359b2fe9a7dc1e4a9a8d41fda803895df5
Ardit Zubaku <zubaku92@gmail.com>
2026-07-23 06:06:59 +0200

    game state

--- game/src/main.rs
+++ game/src/main.rs
@@ -1,13 +1,31 @@
+use std::ptr::null;
+
 use rusty_engine::prelude::*;
 
 #[derive(Resource)]
-struct GameState {}
+struct GameState {
+    high_score: u32,
+    current_score: u32,
+    enemy_labels: Vec<String>,
+    spawn_timer: Timer,
+}
+
+impl Default for GameState {
+    fn default() -> Self {
+        Self {
+            high_score: 0,
+            current_score: 0,
+            enemy_labels: Vec::new(),
+            spawn_timer: Timer::from_seconds(1.0, TimerMode::Once),
+        }
+    }
+}
 
 fn main() {
     let mut game = Game::new();
 
     // setup game here
 
-    let init_state = GameState {};
+    let init_state = GameState::default();
     game.run(init_state);
 }
