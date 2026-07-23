634ee3180dc6e102abb547c037cf16081081f726
Ardit Zubaku <zubaku92@gmail.com>
2026-07-23 04:12:07 +0200

    engine init

--- game/src/main.rs
+++ game/src/main.rs
@@ -1,3 +1,13 @@
+use rusty_engine::prelude::*;
+
+#[derive(Resource)]
+struct GameState {}
+
 fn main() {
-    println!("Hello, world!");
+    let mut game = Game::new();
+
+    // setup game here
+
+    let init_state = GameState {};
+    game.run(init_state);
 }
