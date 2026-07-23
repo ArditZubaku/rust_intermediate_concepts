31500e7cc28a6849307c7fed4dd733484fafe364
Ardit Zubaku <zubaku92@gmail.com>
2026-07-23 06:51:53 +0200

    collisions

--- game/src/main.rs
+++ game/src/main.rs
@@ -8,6 +8,14 @@ struct GameState {
     spawn_timer: Timer,
 }
 
+struct Labels {
+    player: String,
+    car1: String,
+}
+
+const PLAYER_LABEL: &str = "player";
+const CAR1_LABEL: &str = "car1";
+
 impl Default for GameState {
     fn default() -> Self {
         Self {
@@ -22,17 +30,22 @@ impl Default for GameState {
 fn main() {
     let mut game = Game::new();
 
-    let player = game.add_sprite("player", SpritePreset::RacingCarBlue);
+    let player = game.add_sprite(PLAYER_LABEL, SpritePreset::RacingCarBlue);
     player.translation = Vec2::new(0.0, 0.0);
     // player.rotation = std::f32::consts::FRAC_PI_2;
     // player.rotation = UP;
     player.rotation = SOUTH_WEST;
     player.scale = 1.5;
     player.layer = 1.0; // when on the same layer it's non-deterministic
+    player.collision = true;
 
     // let temporary = game.add_sprite("temporary", SpritePreset::RacingCarRed);
     // temporary.translation = Vec2::new(30.0, 0.0);
     // temporary.layer = 999.0; // the highest layer possible to be set
+
+    let car1 = game.add_sprite(CAR1_LABEL, SpritePreset::RacingCarYellow);
+    car1.translation = Vec2::new(300.0, 0.0);
+    car1.collision = true;
 
     game.add_logic(game_logic);
 
@@ -41,5 +54,22 @@ fn main() {
 }
 
 fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
-    // println!("Game state {:?}", game_state);
+    // engine.show_colliders = true;
+    for event in engine.collision_events.drain(..) {
+        // println!("{:?}", event);
+        if event.state == CollisionState::Begin && event.pair.one_starts_with(PLAYER_LABEL) {
+            // remove the sprit the player collided with
+            for label in [event.pair.0, event.pair.1] {
+                if label != PLAYER_LABEL {
+                    engine.sprites.remove(&label);
+                }
+            }
+
+            game_state.current_score += 1;
+            println!("Game state {:?}", game_state);
+        }
+    }
+
+    let player = engine.sprites.get_mut(PLAYER_LABEL).unwrap();
+    player.translation.x += 100.0 * engine.delta_f32;
 }
