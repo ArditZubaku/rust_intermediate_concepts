f03d4c7b69f2f7d074384e8b3f6b86d2b51c2461
Ardit Zubaku <zubaku92@gmail.com>
2026-07-25 18:48:49 +0200

    road race game

--- road_race/src/main.rs
+++ road_race/src/main.rs
@@ -1,16 +1,116 @@
+use rand::RngExt;
 use rusty_engine::prelude::*;
 
 // Define a struct to hold custom data for your game (it can be a lot more complicated than this one!)
-#[derive(Resource)]
-struct GameState {}
+#[derive(Default, Resource)]
+struct GameState {
+    health_amount: u8,
+    lost: bool,
+}
 
 fn main() {
     let mut game = Game::new();
 
+    let player1 = game.add_sprite("player1", SpritePreset::RacingCarBlue);
+    player1.translation.x = -500.0;
+    player1.layer = 10.0;
+    player1.collision = true;
+
+    game.audio_manager
+        .play_music(MusicPreset::WhimsicalPopsicle, 0.2);
+
+    for i in 0..10 {
+        let roadline = game.add_sprite(format!("roadline{}", i), SpritePreset::RacingBarrierWhite);
+        roadline.scale = 0.1;
+        roadline.translation.x = -600.0 + 150.0 * i as f32;
+    }
+
+    let obstacle_presets = vec![
+        SpritePreset::RacingBarrelBlue,
+        SpritePreset::RacingBarrelRed,
+        SpritePreset::RacingConeStraight,
+    ];
+
+    for (i, preset) in obstacle_presets.into_iter().enumerate() {
+        let obstacle = game.add_sprite(format!("obstacle{}", i), preset);
+        obstacle.layer = 5.0;
+        obstacle.collision = true;
+        obstacle.translation.x = rand::rng().random_range(800.0..1600.0);
+        obstacle.translation.y = rand::rng().random_range(-300.0..300.0);
+    }
+
+    let health_message = game.add_text("health_message", "Health: 5");
+    health_message.translation = Vec2::new(550.0, 320.0);
+
     game.add_logic(game_logic);
 
-    let initial_game_state = GameState {};
-    game.run(initial_game_state);
+    game.run(GameState {
+        health_amount: 5,
+        lost: false,
+    });
 }
 
-fn game_logic(engine: &mut Engine, game_state: &mut GameState) {}
+const PLAYER_SPEED: f32 = 250.0; // 250 pixels per second
+const ROAD_SPEED: f32 = 400.0;
+
+fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
+    if game_state.lost {
+        return;
+    }
+
+    let mut direction = 0.0;
+
+    if engine.keyboard_state.pressed(KeyCode::ArrowUp) {
+        direction += 1.0;
+    }
+
+    if engine.keyboard_state.pressed(KeyCode::ArrowDown) {
+        direction -= 1.0;
+    }
+
+    let player1 = engine.sprites.get_mut("player1").unwrap();
+    player1.translation.y += direction * PLAYER_SPEED * engine.delta_f32;
+    player1.rotation = direction * 0.15;
+
+    if player1.translation.y < -360.0 || player1.translation.y > 360.0 {
+        game_state.health_amount = 0;
+    }
+
+    for sprite in engine.sprites.values_mut() {
+        if sprite.label.starts_with("roadline") {
+            sprite.translation.x -= ROAD_SPEED * engine.delta_f32;
+            if sprite.translation.x < -675.0 {
+                sprite.translation.x += 1500.0;
+            }
+        }
+
+        if sprite.label.starts_with("obstacle") {
+            sprite.translation.x -= ROAD_SPEED * engine.delta_f32;
+            if sprite.translation.x < -800.0 {
+                sprite.translation.x = rand::rng().random_range(800.0..1600.0);
+                sprite.translation.y = rand::rng().random_range(-300.0..300.0);
+            }
+        }
+    }
+
+    let health_message = engine.texts.get_mut("health_message").unwrap();
+    for event in engine.collision_events.drain(..) {
+        // We don't care if obstacles collide with each other of if a collision ended
+        if !event.pair.either_contains("player1") || event.state.is_end() {
+            continue;
+        }
+        if game_state.health_amount > 0 {
+            game_state.health_amount -= 1;
+            health_message.value = format!("Health: {}", game_state.health_amount);
+            engine.audio_manager.play_sfx(SfxPreset::Impact1, 0.5);
+        }
+    }
+
+    if game_state.health_amount == 0 {
+        game_state.lost = true;
+        let game_over = engine.add_text("game over", "Game Over");
+        game_over.font_size = 128.0;
+        engine.audio_manager.stop_music();
+        engine.audio_manager.play_sfx(SfxPreset::Jingle3, 0.5);
+    }
+}
